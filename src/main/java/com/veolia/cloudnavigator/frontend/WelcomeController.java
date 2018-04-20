eb0377e57e45489ef18e9f1e5dd361e6b4b27d1b
Philipp Meier <mail@philippmeier.com>
2018-04-20 13:42:28 +0200

    Add cloudsql and local sql connection

--- src/main/java/com/veolia/cloudnavigator/frontend/WelcomeController.java
+++ src/main/java/com/veolia/cloudnavigator/frontend/WelcomeController.java
@@ -2,12 +2,16 @@ package com.veolia.cloudnavigator.frontend;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WelcomeController {
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 
 	@RequestMapping("/_ah/health")
 	@ResponseBody
@@ -18,6 +22,7 @@ public class WelcomeController {
 
 	@RequestMapping("/")
 	public String loginMessage(HttpServletRequest request) {
+		jdbcTemplate.execute("show tables");
 		String email = request.getHeader("X-Goog-Authenticated-User-Email");
 		if (email == null) {
 			// should not happen on app-engine because of IAP proxy
